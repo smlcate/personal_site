41be177a655d69a6ad5910424e28421ddf86a2da
Sam Cate <SamCate@Sams-MacBook-Pro.local>
2017-01-02 17:33:08 -0500

    initialized

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+var express = require('express');
+var app = express();
+var knex = require('./db/knex');
+var bodyParser = require('body-parser');
+
+var server = {
+  main: require('./controllers/main.js'),
+  admin: require('./controllers/admin.js'),
+  checkout: require('./controllers/checkout.js')
+}
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
